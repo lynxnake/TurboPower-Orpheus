223cdd24003bc828444614349ee51a7437363ada
romankassebaum <romankassebaum@fbd941df-1b1f-4d16-88fd-9a14aed2154d>
2010-02-25 10:28:01 +0000

    Added a header for TestOVCEditU.pas
    git-svn-id: https://svn.code.sf.net/p/tporpheus/code/trunk@87 fbd941df-1b1f-4d16-88fd-9a14aed2154d

--- source/dunit/TestOVCEditU.pas
+++ source/dunit/TestOVCEditU.pas
@@ -1,3 +1,19 @@
+{* http://www.mozilla.org/MPL/                                                *}
+{*                                                                            *}
+{* Software distributed under the License is distributed on an "AS IS" basis, *}
+{* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License   *}
+{* for the specific language governing rights and limitations under the       *}
+{* License.                                                                   *}
+{*                                                                            *}
+{* The Original Code is TurboPower Orpheus                                    *}
+{*                                                                            *}
+{* The Initial Developer of the Original Code is Roman Kassebaum              *}
+{*                                                                            *}
+{*                                                                            *}
+{* Contributor(s):                                                            *}
+{*    Roman Kassebaum                                                         *}
+{*                                                                            *}
+{* ***** END LICENSE BLOCK *****                                              *}
 unit TestOVCEditU;
 
 interface
